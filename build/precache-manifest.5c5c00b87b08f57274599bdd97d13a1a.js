8a649752762a00d6b2a81debdec62f815e0d678b
jpburns82 <jpburns82@gmail.com>
2022-01-18 20:15:14 -0800

    commit on master

---
+++ build/precache-manifest.5c5c00b87b08f57274599bdd97d13a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94dae46a29b19d1f61c672b2af931cbc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "560dcc8dcd149f1f5761",
+    "url": "./static/css/2.3870cfe2.chunk.css"
+  },
+  {
+    "revision": "ae19808943d2ce7039ba",
+    "url": "./static/css/main.ea20caf2.chunk.css"
+  },
+  {
+    "revision": "560dcc8dcd149f1f5761",
+    "url": "./static/js/2.3af8ebf5.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "./static/js/2.3af8ebf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae19808943d2ce7039ba",
+    "url": "./static/js/main.07ffb422.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
